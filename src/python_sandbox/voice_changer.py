2391adf2691fb31c04d78103af8da26a405142f8
Placeless <thinkoverheat@gmail.com>
2025-07-04 06:34:37 +0900

    Getting mic channels

--- src/python_sandbox/voice_changer.py
+++ src/python_sandbox/voice_changer.py
@@ -1,16 +1,22 @@
 import pyaudio
 
+def get_mic_index(pa):
+  """マイクチャンネルを取得する関数"""
+  
+  # マイクチャンネル一覧をリストに追加する
+  mic_list = []
+  for i in range(pa.get_device_count()):
+    device_info = pa.get_device_info_by_index(i)
+    num_of_imput_ch = device_info['maxInputChannels']
+    
+    if num_of_imput_ch > 0:
+      mic_list.append(device_info['index'])
+      
+      return mic_list[0]
+    
 # PyAudio を準備する
 pa = pyaudio.PyAudio()
 
-# マイクチャンネル一覧をリストに追加する
-mic_list = []
-for i in range(pa.get_device_count()):
-    device_info = pa.get_device_info_by_index(i)
-    num_of_imput_channels = device_info['maxInputChannels']
-    
-    if num_of_imput_channels > 0:
-        mic_list.append(device_info['index'])
-print(mic_list)
-
-# [0, 1, 4, 5, 9, 10, 14] -> 7 つのマイクチャンネルが発見！
+# マイクチャンネルを自動取得
+index = get_mic_index(pa)
+print(index)
