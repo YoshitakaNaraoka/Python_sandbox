df1101854201994f79925db9b90e16a80ebf774d
Placeless <thinkoverheat@gmail.com>
2025-07-03 15:38:04 +0900

    Delete ipynb & complete mind_reader.py

--- src/python_sandbox/__init__.py
+++ src/python_sandbox/__init__.py
@@ -7,7 +7,19 @@ high = 100
 print(low, high)
 
 for i in range(7):
+    # low と high が同じならループを抜ける
+    if low == high:
+        break
+    
     # コンピューターの推測値を確認
     guess = (low + high) // 2
     print('あなたの数字は', guess, 'より大きいですか？(yes/no)')
     answer = input()
+    
+    # ユーザーの答えにより分岐
+    if answer == "yes":
+        low = guess + 1
+    else:
+        high = guess
+        
+print('あなたの考えている数字は', low, 'ですね！')
