c583625ce037fb151d0a70cc828dcc6e3ca72836
Placeless <thinkoverheat@gmail.com>
2025-07-04 06:27:39 +0900

    Discovered MIC channnels

--- src/python_sandbox/voice_changer.py
+++ src/python_sandbox/voice_changer.py
@@ -3,7 +3,14 @@ import pyaudio
 # PyAudio を準備する
 pa = pyaudio.PyAudio()
 
-# 音声デバイス一覧を表示する
+# マイクチャンネル一覧をリストに追加する
+mic_list = []
 for i in range(pa.get_device_count()):
     device_info = pa.get_device_info_by_index(i)
-    print(device_info)
+    num_of_imput_channels = device_info['maxInputChannels']
+    
+    if num_of_imput_channels > 0:
+        mic_list.append(device_info['index'])
+print(mic_list)
+
+# [0, 1, 4, 5, 9, 10, 14] -> 7 つのマイクチャンネルが発見！
