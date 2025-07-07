9abb028ca57accf586700a60e9b89054f7aea1d1
Placeless <thinkoverheat@gmail.com>
2025-07-08 03:15:39 +0900

    plt smiles

--- src/python_sandbox/chemoinformatics.py
+++ src/python_sandbox/chemoinformatics.py
@@ -1,5 +1,22 @@
 from rdkit import Chem
-from rdkit.Chem import Draw
+from rdkit.Chem.Draw import rdMolDraw2D
+import matplotlib.pyplot as plt
 
 mol = Chem.MolFromSmiles("CCO")
-print(mol)
+
+if mol is None:
+    print('SMILES文字列が無効です')
+else:
+    fig, ax = plt.subplots(figsize=(6, 4))
+    
+    from rdkit.Chem import Draw
+    from io import BytesIO
+    
+    img = Draw.MolToImage(mol, size=(600, 400))
+    
+    ax.imshow(img)
+    ax.axis('off')
+    ax.set_title(mol)
+    
+    plt.tight_layout()
+    plt.show()
